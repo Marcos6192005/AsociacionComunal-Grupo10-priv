7fe679bd374cabf5e61e369a66e22f741d8c882b
Ezzz-Lui <ogn.lui@gmail.com>
2026-09-16 17:16:38 -0600

    feat: enhance administration layout with sidebar, breadcrumb navigation, and tooltip support

--- web-asociacion-comunal/app/(dashboard)/administracion/page.tsx
+++ web-asociacion-comunal/app/(dashboard)/administracion/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center">
             <h1>Administración</h1>
         </div>
     )
