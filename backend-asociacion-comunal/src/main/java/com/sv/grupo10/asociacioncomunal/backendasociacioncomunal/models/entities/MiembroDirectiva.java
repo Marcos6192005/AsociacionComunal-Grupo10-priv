6f13053028cda1a540c8e8c7295838c55abb35f9
Ezzz-Lui <ogn.lui@gmail.com>
2026-09-17 00:55:39 -0600

    feat: add Usuario hierarchy with Vecino and MiembroDirectiva

--- backend-asociacion-comunal/src/main/java/com/sv/grupo10/asociacioncomunal/backendasociacioncomunal/models/entities/MiembroDirectiva.java
+++ backend-asociacion-comunal/src/main/java/com/sv/grupo10/asociacioncomunal/backendasociacioncomunal/models/entities/MiembroDirectiva.java
@@ -1,4 +1,20 @@
 package com.sv.grupo10.asociacioncomunal.backendasociacioncomunal.models.entities;
 
-public class MiembroDirectiva {
+public class MiembroDirectiva extends Usuario {
+
+    private static final long serialVersionUID = 1L;
+    private String cargo;
+
+    public MiembroDirectiva(String nombre, String correo, String password, String cargo){
+        super(nombre, correo, password);
+        this.cargo = cargo;
+    }
+
+    @Override
+    public String getRol(){
+        return "ADMINISTRACION";
+    }
+
+    public String getCargo(){return cargo;}
+    public void setCargo(String cargo){this.cargo = cargo;}
 }
--- backend-asociacion-comunal/src/main/java/com/sv/grupo10/asociacioncomunal/backendasociacioncomunal/models/entities/Usuario.java
+++ backend-asociacion-comunal/src/main/java/com/sv/grupo10/asociacioncomunal/backendasociacioncomunal/models/entities/Usuario.java
@@ -1,4 +1,36 @@
 package com.sv.grupo10.asociacioncomunal.backendasociacioncomunal.models.entities;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.UUID;
+
+public abstract class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String nombre;
+    private String correo;
+    private String password;
+
+    public Usuario(String nombre, String correo, String password) {
+        this.id = UUID.randomUUID().toString();
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+    }
+
+    public abstract String getRol();
+
+    public String getId(){return id;}
+    public void setId(String id){this.id = id;}
+
+    public String getNombre(){return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public String getCorreo(){return correo;}
+    public void setCorreo(String correo){this.correo = correo;}
+
+    public String getPassword(){return password;}
+    public void setPassword(String password){this.password = password;}
+
 }
